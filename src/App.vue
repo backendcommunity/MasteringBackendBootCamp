1b0a55ff890ee932e80db999ce8bc1c5922ecd08
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2024-03-23 23:59:43 +0100

    update  the template

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,8 @@
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+  <header class="w-full flex justify-center p-5  ">
+    <img alt="logo" class=" w-96" src="./assets/logo.svg" />
   </header>
 
   <main>
