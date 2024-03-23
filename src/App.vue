be5d3585ae6888837548bd2f880d847830ec286f
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2024-03-24 00:58:40 +0100

    working on the hero section

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
-<script setup lang="ts">
+<script lang="ts">
+import HeroSection from './components/HeroSection.vue'
+
+export default {
+  components: { HeroSection },
+
+}
 
 </script>
-
 <template>
   <header class="w-full flex justify-center p-5  ">
     <img alt="logo" class=" w-96" src="./assets/logo.svg" />
   </header>
 
-  <main>
-    <TheWelcome />
+  <main class="p-16">
+    <hero-section />
   </main>
 </template>
 
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export default {
+    name: "HeroSection"
+}
+</script>
+
+<template>
+    <section class=" grid grid-cols-2 gap-16">
+        <div class="col-span-1  flex flex-col gap-8">
+            <h1 class="text-5xl font-bold">Neque porro quisquam
+                est qui dolorem ipsum6
+            </h1>
+            <p class="text-2xl">Our comprehensive backend development course
+                will equip you with the skills to take your coding
+                career to the next level.
+            </p>
+            <div class="inline">
+                <button class="px-10 rounded-xl py-4 font-semibold bg-[#242099] text-white inline">
+                    Enroll Now
+                </button>
+            </div>
+        </div>
+        <div class="col-span-1">
+            <img alt="se" class="w-96" src="./assets/vid.svg" />
+        </div>
+    </section>
+</template>
