d2e23b4a3cfaa674e41322d3bf613560d809b493
Nweke kenechukwu christopher <72747787+icodejsx@users.noreply.github.com>
2024-03-26 11:10:42 +0100

    done with the advanage

--- src/components/AdvantageSection.vue
+++ src/components/AdvantageSection.vue
@@ -8,7 +8,7 @@ export default {
 
 
 <template>
-    <section class="flex flex-col items-center  py-20 px-16  gap-20 ">
+    <section class="flex flex-col items-center  py-20 lg:px-16  gap-20 ">
         <div class="w-full lg:w-[600px] flex flex-col gap-6">
             <h1 class="font-bold text-2xl lg:text-4xl text-center">Our Advantage</h1>
             <p class="lg:text-xl text-center">BSed interdum auctor risus, sed fringilla purus lacinia ac. Quisque ut
@@ -16,10 +16,10 @@ export default {
                 porttitor finibus eget vel turpis. Phasellus volutpat purus a enim</p>
         </div>
 
-        <main class="flex lg:flex-row flex-col gap-10 items-center justify-center w-full
+        <main class="flex lg:grid grid-cols-3 flex-col gap-10 items-center justify-center w-full
         ">
-            <div class=" flex  w-2/3 gap-10 h-fit ">
-                <div class="col-span-1  w-1/2 flex flex-col gap-8 p-5 border border-black rounded-2xl bg-white py-10">
+            <div class=" col-span-2 flex flex-col lg:grid grid-cols-2 gap-10 h-fit ">
+                <div class="col-span-1  flex flex-col gap-8 p-5 border border-black rounded-2xl bg-white py-10">
                     <div class="flex flex-col gap-5">
                         <h1 class="font-semibold text-2xl">Other Courses</h1>
                         <ul class="list-disc flex flex-col gap-3 pl-5">
@@ -32,7 +32,7 @@ export default {
                         $3000
                     </b>
                 </div>
-                <div class="col-span-1  w-1/2 flex flex-col gap-8 p-5 border border-black rounded-2xl bg-white py-10">
+                <div class="col-span-1   flex flex-col gap-8 p-5 border border-black rounded-2xl bg-white py-10">
                     <div class="flex flex-col gap-5">
                         <h1 class="font-semibold text-2xl">Other Courses</h1>
                         <ul class="list-disc flex flex-col gap-3 pl-5">
@@ -50,7 +50,7 @@ export default {
 
 
             <div
-                class="col-span-1 w-1/3  flex flex-col gap-8 p-5 border border-black rounded-2xl text-white py-10  bg-gradient-to-tr to-[#322EB0] from-[#8F03A1]">
+                class="col-span-1   flex flex-col gap-8 p-5 border border-black rounded-2xl text-white py-10  bg-gradient-to-tr to-[#322EB0] from-[#8F03A1]">
                 <div class="flex flex-col gap-5">
                     <h1 class="font-semibold text-2xl">Other Courses</h1>
                     <ul class="list-disc flex flex-col gap-3 pl-5">
